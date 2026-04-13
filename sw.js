ded54c1012b28b35c2f1ab4b4946e3c8dde67939
tashamongold <tashalv94@gmail.com>
2026-04-13 13:19:59 -0400

    Change CACHE_NAME to 'mawta-2026-v3'
    Updated cache name to version 3 for service worker.

--- sw.js
+++ sw.js
@@ -1,7 +1,7 @@
 // MAWTA 2026 Service Worker
 // Caches the app so it loads even without cell signal
 
-const CACHE_NAME = 'mawta-2026-v2';
+const CACHE_NAME = 'mawta-2026-v3';
 
 // Files to cache for offline use
 const CACHE_FILES = [
